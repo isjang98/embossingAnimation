54384ed8ac19d5b59bfa2e7768b8663ecc9aa922
Jang insu <isjang98@gmail.com>
2017-09-11 14:16:05 +0900

    Add Test Log

--- app/src/main/java/io/github/isang98/embossinganimation/MainActivity.java
+++ app/src/main/java/io/github/isang98/embossinganimation/MainActivity.java
@@ -2,6 +2,7 @@ package io.github.isang98.embossinganimation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new EmbossingThumbView(this));
+
+        Log.e("TAG", "onCreate ");
     }
 }
